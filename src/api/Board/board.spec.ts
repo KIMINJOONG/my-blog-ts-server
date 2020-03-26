a8f952d9e59b7e5124225b1fca80a155a137a628
KIMINJOONG <ijboym216@gmail.com>
2020-03-26 21:13:06 +0900

    add boards/:id test code

--- src/api/Board/board.spec.ts
+++ src/api/Board/board.spec.ts
@@ -29,3 +29,43 @@ describe("GET boards는", () => {
         await Board.deleteMany({});
     });
 });
+
+describe.only("GET boards/:id는", () => {
+    const boards = [
+        { title: "제목1", content: "내용1", category: 1 },
+        { title: "제목2", content: "내용2", category: 2 },
+        { title: "제목3", content: "내용3", category: 3 }
+    ];
+
+    let boardId: string = "";
+
+    before(async () => {
+        await Board.create(boards);
+        const board = await Board.findOne({ name: "제목1" });
+        if (board) boardId = board.id;
+    });
+
+    describe("성공시", () => {
+        it(`id가 ${boardId}인 객체를 반환한다.`, done => {
+            request(app)
+                .get(`/boards/${boardId}`)
+                .end((err, res) => {
+                    should(res.body.data).have.property("_id", boardId);
+                    done();
+                });
+        });
+    });
+
+    describe("실패시", () => {
+        it("id로 게시글을 찾을 수 없는 경우 404로 응답한다.", done => {
+            request(app)
+                .get("/boards/999")
+                .expect(404)
+                .end(done);
+        });
+    });
+
+    after(async () => {
+        await Board.deleteMany({});
+    });
+});
