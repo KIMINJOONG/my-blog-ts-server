cbb4f627b61605c08e59cd63bff6c8c3d4b51951
KIMINJOONG <ijboym216@gmail.com>
2020-05-21 22:58:31 +0900

    change image file logic

--- src/api/Board/controller.ts
+++ src/api/Board/controller.ts
@@ -3,7 +3,6 @@ import { responseMessage } from "../../responsesMessage";
 import Board from "../../config/models/Board";
 import Hashtag from "../../config/models/Hashtag";
 import { Op } from "sequelize";
-import { checkImage } from "../../utils/file";
 export default {
     index: async (req: Request, res: Response, next: NextFunction) => {
         try {
@@ -35,27 +34,26 @@ export default {
     ): Promise<void | Response> => {
         try {
             const hashtags = req.body.content.match(/#[^\s]+/g);
-            checkImage(req.body.content);
-            // const board: Board = await Board.create({
-            //     title: req.body.title,
-            //     content: req.body.content,
-            // });
+            const board: Board = await Board.create({
+                title: req.body.title,
+                content: req.body.content,
+            });
 
-            // if (hashtags) {
-            //     await Promise.all(
-            //         hashtags.map(async (tag: string) => {
-            //             const newHashtags: Hashtag = await Hashtag.create({
-            //                 name: tag.slice(1).toLowerCase(),
-            //             });
-            //             board.$add("boardHashtag", newHashtags);
-            //             return;
-            //         })
-            //     );
-            // }
-            // await board.save();
-            // return res.json(
-            //     responseMessage({ success: true, message: "" }, board)
-            // );
+            if (hashtags) {
+                await Promise.all(
+                    hashtags.map(async (tag: string) => {
+                        const newHashtags: Hashtag = await Hashtag.create({
+                            name: tag.slice(1).toLowerCase(),
+                        });
+                        board.$add("boardHashtag", newHashtags);
+                        return;
+                    })
+                );
+            }
+            await board.save();
+            return res.json(
+                responseMessage({ success: true, message: "" }, board)
+            );
         } catch (error) {
             console.log("error : ", error);
             return next(error);
