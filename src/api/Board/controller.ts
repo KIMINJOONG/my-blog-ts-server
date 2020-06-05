abd82a0ac98888a27b2c9e8696b16654ee5d883b
KIMINJOONG <ijboym216@gmail.com>
2020-06-05 19:00:55 +0900

    add regex

--- src/api/Board/controller.ts
+++ src/api/Board/controller.ts
@@ -4,193 +4,197 @@ import Board from "../../config/models/Board";
 import Hashtag from "../../config/models/Hashtag";
 import { Op } from "sequelize";
 export default {
-    index: async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            let boards: Board[];
-            const { title } = req.query;
-            if (title) {
-                boards = await Board.findAll({
-                    where: {
-                        title: {
-                            [Op.like]: "%" + title + "%",
-                        },
-                    },
-                });
-            } else {
-                boards = await Board.findAll();
-            }
+  index: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let boards: Board[];
+      const { title } = req.query;
+      if (title) {
+        boards = await Board.findAll({
+          where: {
+            title: {
+              [Op.like]: "%" + title + "%",
+            },
+          },
+        });
+      } else {
+        boards = await Board.findAll();
+      }
 
-            return res.json(
-                responseMessage({ success: true, message: "" }, boards)
-            );
-        } catch (error) {
-            return next(error);
-        }
-    },
-    create: async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void | Response> => {
-        try {
-            const hashtags = req.body.content.match(/#[^\s]+/g);
-            const board: Board = await Board.create({
-                title: req.body.title,
-                content: req.body.content,
+      return res.json(
+        responseMessage({ success: true, message: "" }, boards),
+      );
+    } catch (error) {
+      return next(error);
+    }
+  },
+  create: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void | Response> => {
+    try {
+      const hashtags = req.body.content.match(/#[^\s]+/g);
+      const board: Board = await Board.create({
+        title: req.body.title,
+        content: req.body.content,
+      });
+
+      if (hashtags) {
+        await Promise.all(
+          hashtags.map(async (tag: string) => {
+            tag = tag.replace(/<(.|\n)*?>/g, "");
+            tag = tag.trim();
+            const newHashtags: Hashtag = await Hashtag.create({
+              name: tag.slice(1).toLowerCase(),
             });
+            board.$add("boardHashtag", newHashtags);
+            return;
+          }),
+        );
+      }
+      await board.save();
+      return res.json(
+        responseMessage({ success: true, message: "" }, board),
+      );
+    } catch (error) {
+      console.log("error : ", error);
+      return next(error);
+    }
+  },
 
-            if (hashtags) {
-                await Promise.all(
-                    hashtags.map(async (tag: string) => {
-                        const newHashtags: Hashtag = await Hashtag.create({
-                            name: tag.slice(1).toLowerCase(),
-                        });
-                        board.$add("boardHashtag", newHashtags);
-                        return;
-                    })
-                );
-            }
-            await board.save();
-            return res.json(
-                responseMessage({ success: true, message: "" }, board)
-            );
-        } catch (error) {
-            console.log("error : ", error);
-            return next(error);
-        }
-    },
+  detail: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void | Response> => {
+    const {
+      params: { id },
+    } = req;
+    let parsedInt: number = parseInt(id);
 
-    detail: async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void | Response> => {
-        const {
-            params: { id },
-        } = req;
-        let parsedInt: number = parseInt(id);
+    let error = {
+      status: -1,
+      data: null,
+      message: "",
+    };
 
-        let error = {
-            status: -1,
-            data: null,
-            message: "",
-        };
+    if (Number.isNaN(parsedInt)) {
+      error.status = 404;
+      error.message = "잘못된 요청입니다";
+      return next(error);
+    }
+    try {
+      const board = await Board.findOne({ where: { id: parsedInt } });
+      if (!board) {
+        error.status = 400;
+        error.message = "존재하지 않는 게시글입니다.";
+        return next(error);
+      }
+      return res.json(
+        responseMessage({ success: true, message: "" }, board),
+      );
+    } catch (error) {
+      next(error);
+    }
+  },
+  update: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void | Response> => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const parsedId: number = parseInt(id);
 
-        if (Number.isNaN(parsedInt)) {
-            error.status = 404;
-            error.message = "잘못된 요청입니다";
-            return next(error);
-        }
-        try {
-            const board = await Board.findOne({ where: { id: parsedInt } });
-            if (!board) {
-                error.status = 400;
-                error.message = "존재하지 않는 게시글입니다.";
-                return next(error);
-            }
-            return res.json(
-                responseMessage({ success: true, message: "" }, board)
-            );
-        } catch (error) {
-            next(error);
-        }
-    },
-    update: async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void | Response> => {
-        const { id } = req.params;
-        const { title, content } = req.body;
-        const parsedId: number = parseInt(id);
+    let error = {
+      status: -1,
+      data: null,
+      message: "",
+    };
 
-        let error = {
-            status: -1,
-            data: null,
-            message: "",
-        };
+    if (Number.isNaN(parsedId)) {
+      error.status = 404;
+      error.message = "잘못된 요청입니다";
+      return next(error);
+    }
 
-        if (Number.isNaN(parsedId)) {
-            error.status = 404;
-            error.message = "잘못된 요청입니다";
-            return next(error);
-        }
+    try {
+      const board = await Board.findOne({
+        where: { id: parsedId },
+      });
+      if (!board) {
+        error.status = 400;
+        error.message = "존재하지않는 게시글입니다.";
+        return next(error);
+      }
 
-        try {
-            const board = await Board.findOne({
-                where: { id: parsedId },
+      const hashtags = content.match(/#[^\s]+/g);
+      if (hashtags) {
+        await Promise.all(
+          hashtags.map(async (tag: string) => {
+            tag = tag.replace(/<(.|\n)*?>/g, "");
+            tag = tag.trim();
+            const hashtag: Hashtag | null = await Hashtag.findOne({
+              where: { name: tag.slice(1).toLowerCase() },
             });
-            if (!board) {
-                error.status = 400;
-                error.message = "존재하지않는 게시글입니다.";
-                return next(error);
+            if (!hashtag) {
+              const newHashtags: Hashtag = await Hashtag.create({
+                name: tag.slice(1).toLowerCase(),
+              });
+              board.$add("boardHashtag", newHashtags);
             }
+            return;
+          }),
+        );
+      }
 
-            const hashtags = content.match(/#[^\s]+/g);
-            if (hashtags) {
-                await Promise.all(
-                    hashtags.map(async (tag: string) => {
-                        const hashtag: Hashtag | null = await Hashtag.findOne({
-                            where: { name: tag.slice(1).toLowerCase() },
-                        });
-                        if (!hashtag) {
-                            const newHashtags: Hashtag = await Hashtag.create({
-                                name: tag.slice(1).toLowerCase(),
-                            });
-                            board.$add("boardHashtag", newHashtags);
-                        }
-                        return;
-                    })
-                );
-            }
+      await board.update({ title, content });
+      await board.save();
+      return res.json(
+        responseMessage({ success: true, message: "" }, board),
+      );
+    } catch (error) {
+      console.log("error: ", error);
+      error.status = 404;
+      return next(error);
+    }
+  },
+  destroy: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void | Response> => {
+    const {
+      params: { id },
+    } = req;
 
-            await board.update({ title, content });
-            await board.save();
-            return res.json(
-                responseMessage({ success: true, message: "" }, board)
-            );
-        } catch (error) {
-            console.log("error: ", error);
-            error.status = 404;
-            return next(error);
-        }
-    },
-    destroy: async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ): Promise<void | Response> => {
-        const {
-            params: { id },
-        } = req;
+    let error = {
+      status: -1,
+      data: null,
+      message: "",
+    };
 
-        let error = {
-            status: -1,
-            data: null,
-            message: "",
-        };
+    const parsedId: number = parseInt(id);
+    if (Number.isNaN(parsedId)) {
+      error.status = 404;
+      error.message = "잘못된 요청입니다.";
+      return next(error);
+    }
+    try {
+      const board = await Board.findOne({
+        where: { id: parsedId },
+      });
+      if (!board) {
+        error.status = 400;
+        error.message = "존재하지않는 게시글입니다.";
+        return next(error);
+      }
 
-        const parsedId: number = parseInt(id);
-        if (Number.isNaN(parsedId)) {
-            error.status = 404;
-            error.message = "잘못된 요청입니다.";
-            return next(error);
-        }
-        try {
-            const board = await Board.findOne({
-                where: { id: parsedId },
-            });
-            if (!board) {
-                error.status = 400;
-                error.message = "존재하지않는 게시글입니다.";
-                return next(error);
-            }
-
-            await board.destroy();
-            return res.json(responseMessage({ success: true, message: "" }));
-        } catch (error) {
-            error.status = 404;
-            return next(error);
-        }
-    },
+      await board.destroy();
+      return res.json(responseMessage({ success: true, message: "" }));
+    } catch (error) {
+      error.status = 404;
+      return next(error);
+    }
+  },
 };
