a628a8088df3169c4020183d9fdb9ceb9e11f17c
KIMINJOONG <ijboym216@gmail.com>
2020-06-22 18:23:19 +0900

    change responseMessage, board controller 1. add totalCount in responseMessage 2. add totalCount and change findAll -> findAndCountAll

--- src/responsesMessage.ts
+++ src/responsesMessage.ts
@@ -1,28 +1,30 @@
 interface IResponseMessage {
-    success: boolean;
-    message: string;
+  success: boolean;
+  message: string;
 }
 
 export const responseMessage = (
-    { success, message }: IResponseMessage,
-    data?: any
+  { success, message }: IResponseMessage,
+  data?: any,
+  totalCount?: number,
 ) => {
-    return {
-        success,
-        message,
-        data
-    };
+  return {
+    success,
+    message,
+    data,
+    totalCount,
+  };
 };
 
 interface IResponseError extends Error {
-    status: number;
-    data?: any;
+  status: number;
+  data?: any;
 }
 
 export const errorMessage = ({ status, data, message }: IResponseError) => {
-    return {
-        status,
-        data,
-        message
-    };
+  return {
+    status,
+    data,
+    message,
+  };
 };
