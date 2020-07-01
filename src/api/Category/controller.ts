78ce053e43f3955d774571ce7dc3c5d3b10b2d02
KIMINJOONG <ijboym216@gmail.com>
2020-07-01 23:09:30 +0900

    add destroy function in category controller

--- src/api/Category/controller.ts
+++ src/api/Category/controller.ts
@@ -3,38 +3,76 @@ import { responseMessage } from "../../responsesMessage";
 import Category from "../../config/models/Category";
 
 interface ICategory {
-    code: number;
-    name: string;
+  code: number;
+  name: string;
 }
 export default {
-    index: async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const categories = await Category.findAll();
-            res.json(
-                responseMessage({ success: true, message: "" }, categories)
-            );
-        } catch (e) {
-            console.error(e);
-            next(e);
-        }
-    },
-    create: async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const createCategory: ICategory = req.body;
-            const { code, name } = createCategory;
+  index: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const categories = await Category.findAll();
+      res.json(
+        responseMessage({ success: true, message: "" }, categories),
+      );
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  },
+  create: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const createCategory: ICategory = req.body;
+      const { code, name } = createCategory;
 
-            const category: Category = await Category.create({
-                code,
-                name,
-            });
-            if (category) {
-                res.json(
-                    responseMessage({ success: true, message: "" }, category)
-                );
-            }
-        } catch (e) {
-            console.error(e);
-            next(e);
-        }
-    },
+      const category: Category = await Category.create({
+        code,
+        name,
+      });
+      if (category) {
+        res.json(
+          responseMessage({ success: true, message: "" }, category),
+        );
+      }
+    } catch (e) {
+      console.error(e);
+      next(e);
+    }
+  },
+  destroy: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void | Response> => {
+    const {
+      params: { id },
+    } = req;
+
+    let error = {
+      status: -1,
+      data: null,
+      message: "",
+    };
+
+    const parsedId: number = parseInt(id);
+    if (Number.isNaN(parsedId)) {
+      error.status = 404;
+      error.message = "잘못된 요청입니다.";
+      return next(error);
+    }
+    try {
+      const category: Category | null = await Category.findOne({
+        where: { id: parsedId },
+      });
+      if (!category) {
+        error.status = 400;
+        error.message = "존재하지않는 게시글입니다.";
+        return next(error);
+      }
+
+      await category.destroy();
+      return res.json(responseMessage({ success: true, message: "" }));
+    } catch (error) {
+      error.status = 404;
+      return next(error);
+    }
+  },
 };
--- src/api/Category/index.ts
+++ src/api/Category/index.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.get("/", categoryController.index);
 router.post("/", categoryController.create);
+router.delete("/:id", categoryController.destroy);
 
 export default router;
