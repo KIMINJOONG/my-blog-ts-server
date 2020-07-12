80907d657ef0deea066d1e2c238b59709343078f
KIMINJOONG <ijboym216@gmail.com>
2020-07-12 13:51:46 +0900

    change temporary info mysql

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from "sequelize-typescript";
 
 export const sequelize = new Sequelize({
-    database: "my_blog_test",
-    dialect: "mysql",
-    username: "root",
-    password: "PASSWORD",
-    models: [__dirname + "/models"], // or [Player, Team],
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  dialect: "mysql",
+  username: process.env.MYSQLID,
+  password: process.env.MYSQLPASSWORD,
+  models: [__dirname + "/models"], // or [Player, Team],
 });
