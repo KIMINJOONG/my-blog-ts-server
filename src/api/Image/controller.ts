9c49a30a2e1f5252a0af2db19f7439e3e73f8b6a
KIMINJOONG <ijboym216@gmail.com>
2020-07-20 15:27:21 +0900

    change s3 url

--- src/api/Image/controller.ts
+++ src/api/Image/controller.ts
@@ -4,32 +4,32 @@ import Image from "../../config/models/Image";
 import { removeMulterImage } from "../../utils/file";
 
 export default {
-    upload: async (req: any, res: Response, next: NextFunction) => {
-        // console.log("넘어옴");
-        // for (let file of req.files) {
-        //     const { key, originalname } = file;
-        //     Image.create({key, name: originalname});
-        // }
-        return res.json(req.files.map((v: Express.MulterS3.File) => v.key));
-    },
-    destroy: async (req: Request, res: Response, next: NextFunction) => {
-        const {
-            params: { imageKey },
-        } = req;
-        const fullFileName =
-            "https://kohubi-blog.s3.ap-northeast-2.amazonaws.com/images/" +
-            imageKey;
-        // try {
-        //     await Image.destroy({ where: { key: imageKey } });
-        // } catch (error) {
-        //     // 게시글 수정에서 이미지 파일 삭제가 아닌 업로드중 이미지 삭제는 당연히 에러이므로 상관x
-        // }
-        const param = {
-            Bucket: "kohubi-test/images",
-            Key: imageKey,
-        };
+  upload: async (req: any, res: Response, next: NextFunction) => {
+    // console.log("넘어옴");
+    // for (let file of req.files) {
+    //     const { key, originalname } = file;
+    //     Image.create({key, name: originalname});
+    // }
+    return res.json(req.files.map((v: Express.MulterS3.File) => v.key));
+  },
+  destroy: async (req: Request, res: Response, next: NextFunction) => {
+    const {
+      params: { imageKey },
+    } = req;
+    const fullFileName =
+      "https://kohubi-renual-blog.s3-ap-northeast-1.amazonaws.com/images/" +
+      imageKey;
+    // try {
+    //     await Image.destroy({ where: { key: imageKey } });
+    // } catch (error) {
+    //     // 게시글 수정에서 이미지 파일 삭제가 아닌 업로드중 이미지 삭제는 당연히 에러이므로 상관x
+    // }
+    const param = {
+      Bucket: "kohubi-renual-blog/images",
+      Key: imageKey,
+    };
 
-        const result = await removeMulterImage(param);
-        return res.status(200).json(result);
-    },
+    const result = await removeMulterImage(param);
+    return res.status(200).json(result);
+  },
 };
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -6,26 +6,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const s3 = new aws.S3({
-    accessKeyId: process.env.AWS_KEY,
-    secretAccessKey: process.env.AWS_PRIVATE_KEY,
-    region: "ap-northeast-2",
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+  region: "ap-northeast-1",
 });
 
 export const multerImages = multer({
-    storage: multerS3({
-        s3,
-        acl: "public-read",
-        bucket: "kohubi-test/images",
-    }),
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "kohubi-renual-blog/images",
+  }),
 });
 
 export const removeMulterImage = (param: any) => {
-    s3.deleteObject(param, (err, data) => {
-        if (err) {
-            console.error(err);
-            return err;
-        } else {
-            return data;
-        }
-    });
+  s3.deleteObject(param, (err, data) => {
+    if (err) {
+      console.error(err);
+      return err;
+    } else {
+      return data;
+    }
+  });
 };
