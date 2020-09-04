9a1620604efca262481a1e1a3e7cb93059b7755f
KIMINJOONG <ijboym216@gmail.com>
2020-09-04 17:28:29 +0900

    remove blank

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize-typescript";
 
 export const sequelize = new Sequelize({
-  database: process.env.PRODUCTION ? process.env.MYSQLDATABASE : "my_blog_test",
-  host: process.env.PRODUCTION ? process.env.MYSQLHOST : "localhost",
-  dialect: "mysql",
-  username: process.env.PRODUCTION ? process.env.MYSQLID : "root",
-  password: process.env.PRODUCTION ? process.env.MYSQLPASSWORD : "PASSWORD",
-  models: [__dirname + "/models"], // or [Player, Team],
+    database: process.env.PRODUCTION
+        ? process.env.MYSQLDATABASE
+        : "my_blog_test",
+    host: process.env.PRODUCTION ? process.env.MYSQLHOST : "localhost",
+    dialect: "mysql",
+    username: process.env.PRODUCTION ? process.env.MYSQLID : "root",
+    password: process.env.PRODUCTION ? process.env.MYSQLPASSWORD : "PASSWORD",
+    models: [__dirname + "/models"], // or [Player, Team],
 });
