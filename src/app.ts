fcc16d7b79f1fe18279cb67ddc1fbd5720e60b91
KIMINJOONG <ijboym216@gmail.com>
2020-09-06 20:51:07 +0900

    change setting

--- src/app.ts
+++ src/app.ts
@@ -17,35 +17,48 @@ import expressSession from "express-session";
 
 dotenv.config();
 const app = express();
+const prod = process.env.PRODUCTION;
 
-app.use(
+if (prod === "true") {
+  app.use(morgan("combined"));
+  app.use(
     cors({
-        origin: true,
-        credentials: true,
-    })
-);
+      origin: /kohubi\.xyz$/,
+      credentials: true,
+    }),
+  );
+} else {
+  app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
+}
+
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.set("trust proxy", 1);
 app.use(
-    expressSession({
-        resave: false,
-        saveUninitialized: false,
-        secret: process.env.COOKIE_SECRET!,
-        cookie: {
-            httpOnly: true,
-            secure: false, //https를 쓸때 true
-        },
-        name: "rnbck",
-    })
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET!,
+    cookie: {
+      httpOnly: true,
+      secure: prod === "true", // https를 쓸 때 true
+      domain: prod === "true" ? "kohubi.xyz" : "localhost",
+    },
+  }),
 );
 app.use("/", express.static("uploads"));
-app.use(morgan("dev"));
 sequelize.sync({ force: false, alter: true });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 export interface Err extends Error {
-    status: number;
-    data?: any;
+  status: number;
+  data?: any;
 }
 // catch 404 and forward to error handler
 // app.use((req, res, next) => {
@@ -56,15 +69,15 @@ export interface Err extends Error {
 // });
 
 declare global {
-    namespace Express {
-        interface Request {
-            user: User;
-        }
+  namespace Express {
+    interface Request {
+      user: User;
     }
+  }
 }
 
 app.get("/", (req, res) => {
-    return res.send("api");
+  return res.send("api");
 });
 app.use("/users", users);
 app.use("/boards", boards);
@@ -76,22 +89,22 @@ app.use("/likes", likes);
 
 // error handle
 app.use((err: Err, req: Request, res: Response, next: NextFunction) => {
-    // render the error page
-    res.status(err.status || 500);
+  // render the error page
+  res.status(err.status || 500);
 
-    res.json(
-        responseMessage(
-            {
-                success: false,
-                message: err.message,
-            },
-            err.data
-        )
-    );
+  res.json(
+    responseMessage(
+      {
+        success: false,
+        message: err.message,
+      },
+      err.data,
+    ),
+  );
 });
 
 app.listen(4000, () => {
-    console.log("start");
+  console.log("start");
 });
 
 export default app;
