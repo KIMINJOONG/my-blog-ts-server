84140eefffa64898eb89c1190abca8bd572785cc
KIMINJOONG <ijboym216@gmail.com>
2020-11-10 11:06:55 +0900

    add limit size

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,52 @@
-import express, { Request, Response, NextFunction } from "express";
-import morgan from "morgan";
-import users from "./api/User";
-import boards from "./api/Board";
-import hashtags from "./api/Hashtag";
-import categories from "./api/Category";
-import images from "./api/Image";
-import comments from "./api/Comment";
-import likes from "./api/Like";
-import { responseMessage } from "./responsesMessage";
-import User from "./config/models/User";
-import cors from "cors";
-import { sequelize } from "./config/config";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import expressSession from "express-session";
+import express, { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
+import users from './api/User';
+import boards from './api/Board';
+import hashtags from './api/Hashtag';
+import categories from './api/Category';
+import images from './api/Image';
+import comments from './api/Comment';
+import likes from './api/Like';
+import { responseMessage } from './responsesMessage';
+import User from './config/models/User';
+import cors from 'cors';
+import { sequelize } from './config/config';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import expressSession from 'express-session';
 
 dotenv.config();
 const app = express();
 
 app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  }),
+	cors({
+		origin: true,
+		credentials: true
+	})
 );
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.set("trust proxy", 1);
+app.set('trust proxy', 1);
 app.use(
-  expressSession({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET!,
-    cookie: {
-      httpOnly: true,
-      secure: false, //https를 쓸때 true
-    },
-    name: "rnbck",
-  }),
+	expressSession({
+		resave: false,
+		saveUninitialized: false,
+		secret: process.env.COOKIE_SECRET!,
+		cookie: {
+			httpOnly: true,
+			secure: false //https를 쓸때 true
+		},
+		name: 'rnbck'
+	})
 );
-app.use("/", express.static("uploads"));
-app.use(morgan("dev"));
+app.use('/', express.static('uploads'));
+app.use(morgan('dev'));
 sequelize.sync({ force: false, alter: true });
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 export interface Err extends Error {
-  status: number;
-  data?: any;
+	status: number;
+	data?: any;
 }
 // catch 404 and forward to error handler
 // app.use((req, res, next) => {
@@ -57,42 +57,42 @@ export interface Err extends Error {
 // });
 
 declare global {
-  namespace Express {
-    interface Request {
-      user: User;
-    }
-  }
+	namespace Express {
+		interface Request {
+			user: User;
+		}
+	}
 }
 
-app.get("/", (req, res) => {
-  return res.send("api");
+app.get('/', (req, res) => {
+	return res.send('api');
 });
-app.use("/users", users);
-app.use("/boards", boards);
-app.use("/hashtags", hashtags);
-app.use("/images", images);
-app.use("/categories", categories);
-app.use("/comments", comments);
-app.use("/likes", likes);
+app.use('/users', users);
+app.use('/boards', boards);
+app.use('/hashtags', hashtags);
+app.use('/images', images);
+app.use('/categories', categories);
+app.use('/comments', comments);
+app.use('/likes', likes);
 
 // error handle
 app.use((err: Err, req: Request, res: Response, next: NextFunction) => {
-  // render the error page
-  res.status(err.status || 500);
+	// render the error page
+	res.status(err.status || 500);
 
-  res.json(
-    responseMessage(
-      {
-        success: false,
-        message: err.message,
-      },
-      err.data,
-    ),
-  );
+	res.json(
+		responseMessage(
+			{
+				success: false,
+				message: err.message
+			},
+			err.data
+		)
+	);
 });
 
 app.listen(4000, () => {
-  console.log("start");
+	console.log('start');
 });
 
 export default app;
